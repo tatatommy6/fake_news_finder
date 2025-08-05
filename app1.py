bf72ed1f1f7bc491928d386a7e1479a24b0fca0b
김민결 <139607260+tatatommy6@users.noreply.github.com>
2025-08-05 18:51:36 +0900

    모델 로드

--- app1.py
+++ app1.py
@@ -2,7 +2,21 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import feedparser
+import torch
+
+model_name = "/Users/kimminkyeol/Desktop/fake_news_detect"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+model.eval()
+
+# 클래스 번호 -> 라벨 이름 매핑
+label_map = {
+    0: "FAKE",
+    1: "REAL"
+}
+
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory = "static"), name = "static")
@@ -35,3 +49,29 @@ async def search_news(request: Request):
     except Exception as e:
         print("예외 발생:", e)
         return JSONResponse(status_code=500, content={"error": str(e)})
+    
+
+@app.post("/predict", response_class=HTMLResponse)
+async def post_predict(request: Request):
+    keyword = "네가 index에서 검색한 키워드"  # 또는 form에서 받아올 수도 있음
+    articles = crawl_website(keyword, 5)
+
+    results = []
+    for title in articles:
+        inputs = tokenizer(title, return_tensors="pt", truncation=True, padding=True, max_length=512)
+        with torch.no_grad():
+            outputs = model(**inputs)
+            probs = torch.softmax(outputs.logits, dim=1)
+            pred = torch.argmax(probs, dim=1).item()
+            confidence = probs[0][pred].item()
+        
+        results.append({
+            "title": title,
+            "label": label_map[pred],
+            "confidence": f"{confidence*100:.2f}"
+        })
+
+    return templates.TemplateResponse("predict.html", {
+        "request": request,
+        "results": results
+    })
